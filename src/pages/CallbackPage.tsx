098fdc668fe6875fa6fbe9dbca10c53c11ff4be8
rvmircea <vmircea1771@yahoo.com>
2022-08-18 13:45:02 +0300

    added callback page

--- src/pages/CallbackPage.tsx
+++ src/pages/CallbackPage.tsx
@@ -5,7 +5,8 @@ import { MdOutlineCelebration } from "react-icons/md"
 import { getCart, removeAllFromCart, removeItemsFromCart } from '../api/cartApi';
 import { useAuth0 } from '@auth0/auth0-react';
 import { createOrder } from '../api/orderApi';
-
+import { useQuery } from 'react-query';
+import { Basket } from '../interfaces/IProductList';
 
 let url = document.URL;
 const CallbackPage = () => {
@@ -27,6 +28,7 @@ const CallbackPage = () => {
 
   return (
     <>
+      {status ?
         <main className='flex flex-col items-center justify-center space-y-6 mt-40 md:mt-64'>
           <p className='mx-2 text-center text-lg  md:text-2xl lg:text-3xl'>Transactia a avut loc cu succes. Va multumim!</p>
           <Link to="/" className='md:text-2xl lg:text-xl font-semibold text-orange-600 underline'>
@@ -36,6 +38,13 @@ const CallbackPage = () => {
             <span><MdOutlineCelebration size={50} /></span><span><MdOutlineCelebration size={50} /></span><span><MdOutlineCelebration size={50} /></span>
           </div>
         </main>
+        :
+        <main>
+          <p className='text-xl lg:text-3xl'>Transactia nu a avut loc. Incercati mai tarziu.</p>
+            <Link to="/" className='text-lg lg:text-xl font-semibold text-orange-600 underline'>
+              Inapoi la pagina principala
+            </Link>
+        </main>}
     </>
   )
 }
